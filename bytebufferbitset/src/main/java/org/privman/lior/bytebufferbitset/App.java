97c7d8ef15edec899dd30aea29699e58ab929f3b
Lior Privman <lior.privman@protonmail.com>
2019-10-19 12:25:28 -0400

    initial commit

---
+++ bytebufferbitset/src/main/java/org/privman/lior/bytebufferbitset/App.java
@@ -0,0 +1,13 @@
+package org.privman.lior.bytebufferbitset;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
